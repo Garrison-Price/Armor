d23e56545ab9facea4151638fe8524398bb1eb2e
Garrison Price <garrison.b.price@gmail.com>
2015-04-12 22:40:31 -0700

    Restructured Assets and Added Level Generation Script

---
+++ Assets/Resources/Scripts/LevelGenerator.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class LevelGenerator {
+	private Texture2D levelTemplate;
+	public Texture2D generatedLevel;
+	private GameObject[] mediumTrees;
+	private GameObject[] smallTrees;
+	private GameObject[] rocks;
+	private GameObject ground;
+	private GameObject path;
+	private int currentLevel;
+	private int levelSize;
+	public bool doneGenerating;
+
+	public LevelGenerator(int size) {
+		doneGenerating = false;
+		levelSize = size;
+
+		UnloadAssets ();
+
+		Regex levelRegularExpression = new Regex("(Level)(\\d+)(Scene)",RegexOptions.IgnoreCase|RegexOptions.Singleline);
+		Match match = levelRegularExpression.Match(Application.loadedLevelName);
+
+		if(match.Success) {
+			currentLevel = System.Int32.Parse(match.Groups[2].ToString());
+			LoadAssets();
+			GenerateLevelImage();
+			BuildScene();
+		} else {
+			currentLevel = -1;
+		}
+	}
+
+	private void LoadAssets() {
+		mediumTrees = Resources.LoadAll("Sprites/Scenery/MediumTrees").Cast<GameObject>().ToArray();
+		smallTrees = Resources.LoadAll("Sprites/Scenery/SmallTrees").Cast<GameObject>().ToArray();
+		rocks = Resources.LoadAll("Sprites/Scenery/Rocks").Cast<GameObject>().ToArray();
+		ground = Resources.Load("Sprites/Scenery/Ground") as GameObject;
+		path = Resources.Load("Sprites/Scenery/Path") as GameObject;
+		levelTemplate = Resources.Load("Art/Levels/Level"+currentLevel+"Path") as Texture2D;
+	}
+
+	private void UnloadAssets() {
+		mediumTrees = null;
+		smallTrees = null;
+		rocks = null;
+		ground = null;
+		path = null;
+		Resources.UnloadUnusedAssets();
+	}
+
+	private void GenerateLevelImage() {
+		generatedLevel = new Texture2D(levelSize, levelSize);
+		float increment = .018f * 256 / levelSize; //I like this density at 256 pixels so lets just make the density the same for every size
+		float xoff = 0.0f;
+		Color[] pixels = new Color[levelSize * levelSize];
+		for(int x = 0; x < levelSize; x++)
+		{
+			xoff += increment;
+			float yoff = 0.0f;
+			for(int y = 0; y < levelSize; y++)
+			{
+				yoff += increment;
+				float noise = Mathf.PerlinNoise(xoff + currentLevel * 15,yoff + currentLevel * 15); //Need to offset for each level otherwise it would be the same noise
+				noise = levelTemplate.GetPixel((int)Mathf.Floor(x * 32.0f / levelSize),(int)Mathf.Floor(y * 32.0f / levelSize)).grayscale - noise;
+				noise = Mathf.Min(1.0f,noise);
+				if(noise > .6f)			//This ended up looking good for a threshold
+					noise = .85f;
+				else if(noise <= .6f)
+					noise = 0.0f;
+				pixels[x + y * levelSize] = new Color(noise,noise,noise);
+			}
+		}
+		generatedLevel.SetPixels(pixels);
+		generatedLevel.Apply();
+	}
+
+	private void BuildScene() {
+
+	}
+}
---
+++ Assets/Resources/Scripts/SceneManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class SceneManager : MonoBehaviour {
+	string previousScene = "MainMenu";
+	private static SceneManager _sm;
+	private Dictionary<string, UnityEngine.Events.UnityAction<int>> functionMap;
+	LevelGenerator l;
+	
+	void Awake() {
+		if(_sm == null) {
+			//First instance of the scene manager so make this the only one
+			_sm = this;
+			//Make the scene manager persist in all scenes
+			DontDestroyOnLoad(_sm.gameObject);
+		} else {
+			//Not the first instance
+			if(_sm != this) //I am not the scene manager, destroy me
+				Destroy(this.gameObject);
+		}
+		LoadDictionary();
+		LoadButtons();
+	}
+
+	void OnLevelWasLoaded() {
+		LoadButtons();
+		l = new LevelGenerator(64);
+	}
+
+	//This function creates and initializes the dictionary of buttons.
+	//This is where you should add the names of the buttons and which functions you would like to call when pressed.
+	public void LoadDictionary() {
+		if(_sm.functionMap == null) {
+			//We haven't created and loaded the dictionary yet, so lets do that.
+			functionMap = new Dictionary<string, UnityEngine.Events.UnityAction<int>>();
+			functionMap.Add("LevelSelectButton",LevelSelectButtonClicked);
+			functionMap.Add("OptionsButton",OptionsButtonClicked);
+			functionMap.Add("CreditsButton",CreditsButtonClicked);
+			functionMap.Add("QuitButton",QuitButtonClicked);
+			functionMap.Add("BackButton",BackButtonClicked);
+		}
+	}
+
+	//Initializes all scene changing buttons and their on click methods.
+	public void LoadButtons() {
+		//Setup regular expression for finding level selection buttons
+		Regex levelRegularExpression = new Regex("(Level)(\\d+)(Button)",RegexOptions.IgnoreCase|RegexOptions.Singleline);
+		UnityEngine.UI.Button[] buttons = Resources.FindObjectsOfTypeAll(typeof(UnityEngine.UI.Button)) as UnityEngine.UI.Button[];
+		foreach(UnityEngine.UI.Button button in buttons) {
+			button.onClick.RemoveAllListeners();
+			if(_sm.functionMap.ContainsKey(button.name)) {
+				UnityEditor.Events.UnityEventTools.AddIntPersistentListener(button.onClick, _sm.functionMap[button.name], 1);
+			} else {
+				//See if we found a match for a level selection button
+				Match match = levelRegularExpression.Match(button.name);
+				if(match.Success) //Found one!
+					UnityEditor.Events.UnityEventTools.AddIntPersistentListener(button.onClick, SelectLevelButtonClicked, System.Int32.Parse(match.Groups[2].ToString()));
+			}
+		}
+	}
+
+	//Input is not used here but is required for Dictionary
+	public void BackButtonClicked(int ambiguous) {
+		//Go Back to the previous scene... this may need some work when returning to a game scene.
+		Application.LoadLevel(previousScene);
+	}
+
+	//Button clicked to select and play a level
+	public void SelectLevelButtonClicked(int level) {
+		Application.LoadLevel(string.Concat("Level",level,"Scene"));
+	}
+
+	//Button clicked to go to the level selection menu
+	//Input is not used here but is required for Dictionary
+	public void LevelSelectButtonClicked(int ambiguous) {
+		previousScene = Application.loadedLevelName;
+		Application.LoadLevel("LevelSelectScene");
+	}
+
+	//Button clicked to open the options menu
+	//Input is not used here but is required for Dictionary
+	public void OptionsButtonClicked(int ambiguous) {
+		previousScene = Application.loadedLevelName;
+		Application.LoadLevel("OptionsScene");
+	}
+
+	//Button clicked to view the credits
+	//Input is not used here but is required for Dictionary
+	public void CreditsButtonClicked(int ambiguous) {
+		previousScene = Application.loadedLevelName;
+		Application.LoadLevel("CreditsScene");
+	}
+
+	//Button clicked to quit the game.
+	//Input is not used here but is required for Dictionary
+	public void QuitButtonClicked(int ambiguous) {
+		Application.Quit();
+	}
+}
